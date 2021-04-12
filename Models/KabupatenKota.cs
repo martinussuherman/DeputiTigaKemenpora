b9a2b19860ce8ac05f8d08ec7446a27ad5111c00
Martinus Suherman <martinus.suherman@gmail.com>
2021-04-12 11:32:17 +0700

    Add models: Kabupaten/Kota and Provinsi

---
+++ Models/KabupatenKota.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace DeputiTigaKemenpora.Models
+{
+    public partial class KabupatenKota
+    {
+        public KabupatenKota()
+        {
+            Kegiatan = new HashSet<Kegiatan>();
+        }
+
+        public int Kode { get; set; }
+        public string Nama { get; set; }
+        public sbyte KodeProvinsi { get; set; }
+        public decimal Lat { get; set; }
+        public decimal Long { get; set; }
+
+        public virtual Provinsi KodeProvinsiNavigation { get; set; }
+        public virtual ICollection<Kegiatan> Kegiatan { get; set; }
+    }
+}
---
+++ Models/Provinsi.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace DeputiTigaKemenpora.Models
+{
+    public partial class Provinsi
+    {
+        public Provinsi()
+        {
+            KabupatenKota = new HashSet<KabupatenKota>();
+        }
+
+        public sbyte Kode { get; set; }
+        public string Nama { get; set; }
+        public decimal Lat { get; set; }
+        public decimal Long { get; set; }
+
+        public virtual ICollection<KabupatenKota> KabupatenKota { get; set; }
+    }
+}
