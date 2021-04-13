9663141a903d761e99e301edf2eef1efc844da70
Martinus Suherman <martinus.suherman@gmail.com>
2021-04-13 10:46:53 +0700

    Create Provinsi view model

---
+++ ViewModels/Provinsi.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DeputiTigaKemenpora.ViewModels
+{
+    public class Provinsi : IModels<Models.Provinsi>
+    {
+        public sbyte Kode
+        {
+            get => Models.Kode;
+            set => Models.Kode = value;
+        }
+
+        [Required(ErrorMessage = "Nama harus diisi.")]
+        public string Nama
+        {
+            get => Models.Nama;
+            set => Models.Nama = value;
+        }
+
+        [Required(ErrorMessage = "Lintang harus diisi.")]
+        public decimal Lat
+        {
+            get => Models.Lat;
+            set => Models.Lat = value;
+        }
+
+        [Required(ErrorMessage = "Bujur harus diisi.")]
+        public decimal Long
+        {
+            get => Models.Long;
+            set => Models.Long = value;
+        }
+
+        public Models.Provinsi Models { get; set; } = new Models.Provinsi();
+    }
+}
