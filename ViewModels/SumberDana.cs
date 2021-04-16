af4bbd9a2593a511746997bc136c5e56c4d4e56b
Martinus Suherman <martinus.suherman@gmail.com>
2021-04-16 15:14:30 +0700

    Create sumber dana view model

---
+++ ViewModels/SumberDana.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DeputiTigaKemenpora.ViewModels
+{
+   public class SumberDana : IModels<Models.SumberDana>
+   {
+      public byte Id
+      {
+         get => Models.Id;
+         set => Models.Id = value;
+      }
+
+      [Required(ErrorMessage = "Nama harus diisi.")]
+      public string Nama
+      {
+         get => Models.Nama;
+         set => Models.Nama = value;
+      }
+
+      public Models.SumberDana Models { get; set; } = new Models.SumberDana();
+   }
+}
