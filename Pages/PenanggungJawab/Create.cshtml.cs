7032d756d6da9a1f6708ecda6b26d2ead5661670
Martinus Suherman <martinus.suherman@gmail.com>
2021-04-16 15:05:49 +0700

    Update penanggung jawab create/edit page model
    - Use interface for page model
    - Remove logger DI
    - Remove unnecessary using
    - Reformat code

--- Pages/PenanggungJawab/Create.cshtml.cs
+++ Pages/PenanggungJawab/Create.cshtml.cs
@@ -1,48 +1,42 @@
-using System;
 using System.Threading.Tasks;
 using DeputiTigaKemenpora.Data;
 using Itm.Misc;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 
 namespace DeputiTigaKemenpora.Pages.PenanggungJawab
 {
-    [Authorize]
-    public class CreateModel : CustomPageModel
-    {
-        public CreateModel(
-            ILogger<CreateModel> logger,
-            ApplicationDbContext context)
-        {
-            _logger = logger;
-            _context = context;
-            Title = "Tambah Penanggung Jawab";
-            PageTitle = "Penanggung Jawab";
-        }
+   [Authorize]
+   public class CreateModel : CustomPageModel, IPenanggungJawabEditModel
+   {
+      public CreateModel(ApplicationDbContext context)
+      {
+         _context = context;
+         Title = "Tambah Penanggung Jawab";
+         PageTitle = "Penanggung Jawab";
+      }
 
-        [BindProperty]
-        public ViewModels.PenanggungJawab PenanggungJawab { get; set; }
+      [BindProperty]
+      public ViewModels.PenanggungJawab PenanggungJawab { get; set; }
 
-        public IActionResult OnGet()
-        {
+      public IActionResult OnGet()
+      {
+         return Page();
+      }
+
+      public async Task<IActionResult> OnPostAsync()
+      {
+         if (!ModelState.IsValid)
+         {
             return Page();
-        }
+         }
 
-        public async Task<IActionResult> OnPostAsync()
-        {
-            if (!ModelState.IsValid)
-            {
-                return Page();
-            }
+         _context.PenanggungJawab.Add(PenanggungJawab.Models);
+         await _context.SaveChangesAsync();
 
-            _context.PenanggungJawab.Add(PenanggungJawab.Models);
-            await _context.SaveChangesAsync();
+         return RedirectToPage("./Index");
+      }
 
-            return RedirectToPage("./Index");
-        }
-
-        private readonly ILogger<CreateModel> _logger;
-        private readonly ApplicationDbContext _context;
-    }
+      private readonly ApplicationDbContext _context;
+   }
 }
--- Pages/PenanggungJawab/Edit.cshtml.cs
+++ Pages/PenanggungJawab/Edit.cshtml.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Linq;
 using System.Threading.Tasks;
 using DeputiTigaKemenpora.Data;
@@ -6,79 +5,74 @@ using Itm.Misc;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Logging;
 
 namespace DeputiTigaKemenpora.Pages.PenanggungJawab
 {
-    [Authorize]
-    public class EditModel : CustomPageModel
-    {
-        public EditModel(
-            ILogger<EditModel> logger,
-            ApplicationDbContext context)
-        {
-            _logger = logger;
-            _context = context;
-            Title = "Ubah Penanggung Jawab";
-            PageTitle = "Penanggung Jawab";
-        }
+   [Authorize]
+   public class EditModel : CustomPageModel, IPenanggungJawabEditModel
+   {
+      public EditModel(ApplicationDbContext context)
+      {
+         _context = context;
+         Title = "Ubah Penanggung Jawab";
+         PageTitle = "Penanggung Jawab";
+      }
 
-        [BindProperty]
-        public ViewModels.PenanggungJawab PenanggungJawab { get; set; } = new ViewModels.PenanggungJawab();
+      [BindProperty]
+      public ViewModels.PenanggungJawab PenanggungJawab { get; set; } = new ViewModels.PenanggungJawab();
 
-        public async Task<IActionResult> OnGetAsync(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
+      public async Task<IActionResult> OnGetAsync(int? id)
+      {
+         if (id == null)
+         {
+            return NotFound();
+         }
 
-            PenanggungJawab.Models = await _context.PenanggungJawab
-                .AsNoTracking()
-                .FirstOrDefaultAsync(e => e.Id == id);
+         PenanggungJawab.Models = await _context.PenanggungJawab
+             .AsNoTracking()
+             .FirstOrDefaultAsync(e => e.Id == id);
 
-            if (PenanggungJawab.Models == null)
-            {
-                return NotFound();
-            }
+         if (PenanggungJawab.Models == null)
+         {
+            return NotFound();
+         }
 
+         return Page();
+      }
+
+      public async Task<IActionResult> OnPostAsync()
+      {
+         if (!ModelState.IsValid)
+         {
             return Page();
-        }
+         }
 
-        public async Task<IActionResult> OnPostAsync()
-        {
-            if (!ModelState.IsValid)
+         _context.Attach(PenanggungJawab.Models).State = EntityState.Modified;
+
+         try
+         {
+            await _context.SaveChangesAsync();
+         }
+         catch (DbUpdateConcurrencyException)
+         {
+            if (!Exists(PenanggungJawab.Id))
             {
-                return Page();
+               return NotFound();
             }
-
-            _context.Attach(PenanggungJawab.Models).State = EntityState.Modified;
-
-            try
+            else
             {
-                await _context.SaveChangesAsync();
+               throw;
             }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!Exists(PenanggungJawab.Id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
+         }
 
-            return RedirectToPage("./Index");
-        }
+         return RedirectToPage("./Index");
+      }
 
-        private bool Exists(int id)
-        {
-            return _context.PenanggungJawab.Any(e => e.Id == id);
-        }
+      private bool Exists(int id)
+      {
+         return _context.PenanggungJawab.Any(e => e.Id == id);
+      }
 
-        private readonly ILogger<EditModel> _logger;
-        private readonly ApplicationDbContext _context;
-    }
+      private readonly ApplicationDbContext _context;
+   }
 }
