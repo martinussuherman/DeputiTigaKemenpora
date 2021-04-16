2177707d34e57978c7b9cfe9788efbce8633d714
Martinus Suherman <martinus.suherman@gmail.com>
2021-04-16 14:59:56 +0700

    Add model for sumber dana

---
+++ Models/SumberDana.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace DeputiTigaKemenpora.Models
+{
+   public partial class SumberDana
+   {
+      public SumberDana()
+      {
+         Kegiatan = new HashSet<Kegiatan>();
+      }
+
+      public byte Id { get; set; }
+      public string Nama { get; set; }
+
+      public virtual ICollection<Kegiatan> Kegiatan { get; set; }
+   }
+}
