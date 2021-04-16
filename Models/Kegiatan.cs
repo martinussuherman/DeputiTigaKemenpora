9cdf8fdfe98281bd9e2586f1fad6da23e3b3550e
Martinus Suherman <martinus.suherman@gmail.com>
2021-04-16 15:01:06 +0700

    Update kegiatan model
    - Add sumber dana field
    - Reformat code

--- Models/Kegiatan.cs
+++ Models/Kegiatan.cs
@@ -2,32 +2,34 @@
 
 namespace DeputiTigaKemenpora.Models
 {
-    public partial class Kegiatan
-    {
-        public uint Id { get; set; }
-        public ushort? PenanggungJawab { get; set; }
-        public string Nama { get; set; }
-        public string Tempat { get; set; }
-        public DateTime TanggalMulai { get; set; }
-        public DateTime TanggalSelesai { get; set; }
-        public string NamaPejabatPembuka { get; set; }
-        public uint JumlahPeserta { get; set; }
-        public sbyte AdaKendala { get; set; }
-        public string Kendala { get; set; }
-        public int? KabupatenKota { get; set; }
-        public string LinkBerita1 { get; set; }
-        public string LinkBerita2 { get; set; }
-        public string LinkBerita3 { get; set; }
-        public string FotoKegiatan1 { get; set; }
-        public string FotoKegiatan2 { get; set; }
-        public string FotoKegiatan3 { get; set; }
-        public string FotoKegiatan4 { get; set; }
-        public string FotoKegiatan5 { get; set; }
-        public string FilePendukung1 { get; set; }
-        public string FilePendukung2 { get; set; }
-        public string FilePendukung3 { get; set; }
+   public partial class Kegiatan
+   {
+      public uint Id { get; set; }
+      public ushort? PenanggungJawab { get; set; }
+      public int? KabupatenKota { get; set; }
+      public byte? SumberDana { get; set; }
+      public string Nama { get; set; }
+      public string Tempat { get; set; }
+      public DateTime TanggalMulai { get; set; }
+      public DateTime TanggalSelesai { get; set; }
+      public string NamaPejabatPembuka { get; set; }
+      public uint JumlahPeserta { get; set; }
+      public sbyte AdaKendala { get; set; }
+      public string Kendala { get; set; }
+      public string LinkBerita1 { get; set; }
+      public string LinkBerita2 { get; set; }
+      public string LinkBerita3 { get; set; }
+      public string FotoKegiatan1 { get; set; }
+      public string FotoKegiatan2 { get; set; }
+      public string FotoKegiatan3 { get; set; }
+      public string FotoKegiatan4 { get; set; }
+      public string FotoKegiatan5 { get; set; }
+      public string FilePendukung1 { get; set; }
+      public string FilePendukung2 { get; set; }
+      public string FilePendukung3 { get; set; }
 
-        public virtual KabupatenKota KabupatenKotaNavigation { get; set; }
-        public virtual PenanggungJawab PenanggungJawabNavigation { get; set; }
-    }
+      public virtual KabupatenKota KabupatenKotaNavigation { get; set; }
+      public virtual PenanggungJawab PenanggungJawabNavigation { get; set; }
+      public virtual SumberDana SumberDanaNavigation { get; set; }
+   }
 }
