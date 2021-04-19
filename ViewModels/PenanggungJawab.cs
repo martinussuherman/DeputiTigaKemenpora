5b175d2e4acecae8f94e2c0a67b58f5b4fb72276
Martinus Suherman <martinus.suherman@gmail.com>
2021-04-19 13:36:51 +0700

    Reformat code: Penanggung Jawab view model

--- ViewModels/PenanggungJawab.cs
+++ ViewModels/PenanggungJawab.cs
@@ -2,21 +2,21 @@
 
 namespace DeputiTigaKemenpora.ViewModels
 {
-    public class PenanggungJawab : IModels<Models.PenanggungJawab>
-    {
-        public ushort Id
-        {
-            get => Models.Id;
-            set => Models.Id = value;
-        }
+   public class PenanggungJawab : IModels<Models.PenanggungJawab>
+   {
+      public ushort Id
+      {
+         get => Models.Id;
+         set => Models.Id = value;
+      }
 
-        [Required(ErrorMessage = "Nama harus diisi.")]
-        public string Nama
-        {
-            get => Models.Nama;
-            set => Models.Nama = value;
-        }
+      [Required(ErrorMessage = "Nama harus diisi.")]
+      public string Nama
+      {
+         get => Models.Nama;
+         set => Models.Nama = value;
+      }
 
-        public Models.PenanggungJawab Models { get; set; } = new Models.PenanggungJawab();
-    }
+      public Models.PenanggungJawab Models { get; set; } = new Models.PenanggungJawab();
+   }
 }
