0148f569d5c4a2e6c671ae1308ca19c5aa32e0d2
Martinus Suherman <martinus.suherman@gmail.com>
2021-04-19 12:28:26 +0700

    Reformat code: Penanggung Jawab model

--- Models/PenanggungJawab.cs
+++ Models/PenanggungJawab.cs
@@ -2,16 +2,16 @@
 
 namespace DeputiTigaKemenpora.Models
 {
-    public partial class PenanggungJawab
-    {
-        public PenanggungJawab()
-        {
-            Kegiatan = new HashSet<Kegiatan>();
-        }
+   public partial class PenanggungJawab
+   {
+      public PenanggungJawab()
+      {
+         Kegiatan = new HashSet<Kegiatan>();
+      }
 
-        public ushort Id { get; set; }
-        public string Nama { get; set; }
+      public ushort Id { get; set; }
+      public string Nama { get; set; }
 
-        public virtual ICollection<Kegiatan> Kegiatan { get; set; }
-    }
+      public virtual ICollection<Kegiatan> Kegiatan { get; set; }
+   }
 }
