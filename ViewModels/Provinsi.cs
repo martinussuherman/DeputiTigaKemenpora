d262dc7bb5bce6e17953683ef9080f65cfc00652
Martinus Suherman <martinus.suherman@gmail.com>
2021-04-19 13:39:29 +0700

    Update Provinsi view model
    - Rename field Kode to Id
    - Change field Id type from sbyte to byte
    - Reformat code

--- ViewModels/Provinsi.cs
+++ ViewModels/Provinsi.cs
@@ -2,35 +2,35 @@
 
 namespace DeputiTigaKemenpora.ViewModels
 {
-    public class Provinsi : IModels<Models.Provinsi>
-    {
-        public sbyte Kode
-        {
-            get => Models.Kode;
-            set => Models.Kode = value;
-        }
+   public class Provinsi : IModels<Models.Provinsi>
+   {
+      public byte Id
+      {
+         get => Models.Id;
+         set => Models.Id = value;
+      }
 
-        [Required(ErrorMessage = "Nama harus diisi.")]
-        public string Nama
-        {
-            get => Models.Nama;
-            set => Models.Nama = value;
-        }
+      [Required(ErrorMessage = "Nama harus diisi.")]
+      public string Nama
+      {
+         get => Models.Nama;
+         set => Models.Nama = value;
+      }
 
-        [Required(ErrorMessage = "Lintang harus diisi.")]
-        public decimal Lat
-        {
-            get => Models.Lat;
-            set => Models.Lat = value;
-        }
+      [Required(ErrorMessage = "Lintang harus diisi.")]
+      public decimal Lat
+      {
+         get => Models.Lat;
+         set => Models.Lat = value;
+      }
 
-        [Required(ErrorMessage = "Bujur harus diisi.")]
-        public decimal Long
-        {
-            get => Models.Long;
-            set => Models.Long = value;
-        }
+      [Required(ErrorMessage = "Bujur harus diisi.")]
+      public decimal Long
+      {
+         get => Models.Long;
+         set => Models.Long = value;
+      }
 
-        public Models.Provinsi Models { get; set; } = new Models.Provinsi();
-    }
+      public Models.Provinsi Models { get; set; } = new Models.Provinsi();
+   }
 }
