b459f76def85701308a6c3142bcc4ada52ac72ad
Martinus Suherman <martinus.suherman@gmail.com>
2021-04-19 12:27:31 +0700

    Update Kabupaten/Kota model
    - Rename field Kode to Id, KodeProvinsi to ProvinsiId
    - Change field Id type from int to ushort
    - Change field ProvinsiId type from sbyte to byte?
    - Reformat code

--- Models/KabupatenKota.cs
+++ Models/KabupatenKota.cs
@@ -2,20 +2,20 @@
 
 namespace DeputiTigaKemenpora.Models
 {
-    public partial class KabupatenKota
-    {
-        public KabupatenKota()
-        {
-            Kegiatan = new HashSet<Kegiatan>();
-        }
+   public partial class KabupatenKota
+   {
+      public KabupatenKota()
+      {
+         Kegiatan = new HashSet<Kegiatan>();
+      }
 
-        public int Kode { get; set; }
-        public string Nama { get; set; }
-        public sbyte KodeProvinsi { get; set; }
-        public decimal Lat { get; set; }
-        public decimal Long { get; set; }
+      public ushort Id { get; set; }
+      public string Nama { get; set; }
+      public byte? ProvinsiId { get; set; }
+      public decimal Lat { get; set; }
+      public decimal Long { get; set; }
 
-        public virtual Provinsi Provinsi { get; set; }
-        public virtual ICollection<Kegiatan> Kegiatan { get; set; }
-    }
+      public virtual Provinsi Provinsi { get; set; }
+      public virtual ICollection<Kegiatan> Kegiatan { get; set; }
+   }
 }
