abf29e3bd708f41ea3842ff28ad855ba6b9256bd
Martinus Suherman <martinus.suherman@gmail.com>
2021-04-19 13:05:09 +0700

    Update Kabupaten/Kota view model
    - Rename field Kode to Id, Provinsi to IdProvinsi
    - Change field type for Id, IdProvinsi
    - Reformat code

--- ViewModels/KabupatenKota.cs
+++ ViewModels/KabupatenKota.cs
@@ -2,47 +2,47 @@
 
 namespace DeputiTigaKemenpora.ViewModels
 {
-    public class KabupatenKota : IModels<Models.KabupatenKota>
-    {
-        public int Kode
-        {
-            get => Models.Kode;
-            set => Models.Kode = value;
-        }
+   public class KabupatenKota : IModels<Models.KabupatenKota>
+   {
+      public ushort Id
+      {
+         get => Models.Id;
+         set => Models.Id = value;
+      }
 
-        [Required(ErrorMessage = "Nama harus diisi.")]
-        public string Nama
-        {
-            get => Models.Nama;
-            set => Models.Nama = value;
-        }
+      [Required(ErrorMessage = "Nama harus diisi.")]
+      public string Nama
+      {
+         get => Models.Nama;
+         set => Models.Nama = value;
+      }
 
-        [Required(ErrorMessage = "Mohon pilih Provinsi.")]
-        public sbyte Provinsi
-        {
-            get => Models.KodeProvinsi;
-            set => Models.KodeProvinsi = value;
-        }
+      [Required(ErrorMessage = "Mohon pilih Provinsi.")]
+      public byte? IdProvinsi
+      {
+         get => Models.ProvinsiId;
+         set => Models.ProvinsiId = value;
+      }
 
-        public string NamaProvinsi
-        {
-            get => Models?.Provinsi?.Nama ?? string.Empty;
-        }
+      public string NamaProvinsi
+      {
+         get => Models?.Provinsi?.Nama ?? string.Empty;
+      }
 
-        [Required(ErrorMessage = "Lintang harus diisi.")]
-        public decimal Lat
-        {
-            get => Models.Lat;
-            set => Models.Lat = value;
-        }
+      [Required(ErrorMessage = "Lintang harus diisi.")]
+      public decimal Lat
+      {
+         get => Models.Lat;
+         set => Models.Lat = value;
+      }
 
-        [Required(ErrorMessage = "Bujur harus diisi.")]
-        public decimal Long
-        {
-            get => Models.Long;
-            set => Models.Long = value;
-        }
+      [Required(ErrorMessage = "Bujur harus diisi.")]
+      public decimal Long
+      {
+         get => Models.Long;
+         set => Models.Long = value;
+      }
 
-        public Models.KabupatenKota Models { get; set; } = new Models.KabupatenKota();
-    }
+      public Models.KabupatenKota Models { get; set; } = new Models.KabupatenKota();
+   }
 }
