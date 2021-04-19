c1bd1ef6eeb885d76305259459a52bb9d742d834
Martinus Suherman <martinus.suherman@gmail.com>
2021-04-19 12:25:28 +0700

    Update provinsi model
    - Rename field Kode to Id
    - Change Id type from sbyte to byte
    - Reformat code

--- Models/Provinsi.cs
+++ Models/Provinsi.cs
@@ -2,18 +2,18 @@
 
 namespace DeputiTigaKemenpora.Models
 {
-    public partial class Provinsi
-    {
-        public Provinsi()
-        {
-            KabupatenKota = new HashSet<KabupatenKota>();
-        }
+   public partial class Provinsi
+   {
+      public Provinsi()
+      {
+         KabupatenKota = new HashSet<KabupatenKota>();
+      }
 
-        public sbyte Kode { get; set; }
-        public string Nama { get; set; }
-        public decimal Lat { get; set; }
-        public decimal Long { get; set; }
+      public byte Id { get; set; }
+      public string Nama { get; set; }
+      public decimal Lat { get; set; }
+      public decimal Long { get; set; }
 
-        public virtual ICollection<KabupatenKota> KabupatenKota { get; set; }
-    }
+      public virtual ICollection<KabupatenKota> KabupatenKota { get; set; }
+   }
 }
