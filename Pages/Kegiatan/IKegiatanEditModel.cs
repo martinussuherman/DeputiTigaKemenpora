780ba4d7c81ad31087659a1dbe4755455d6c7aa4
Martinus Suherman <martinus.suherman@gmail.com>
2021-04-20 12:33:39 +0700

    Update kegiatan page model interface
    - Add penanggung jawab, kabupaten/kota, and sumber dana select list

--- Pages/Kegiatan/IKegiatanEditModel.cs
+++ Pages/Kegiatan/IKegiatanEditModel.cs
@@ -1,7 +1,15 @@
+using Microsoft.AspNetCore.Mvc.Rendering;
+
 namespace DeputiTigaKemenpora.Pages.Kegiatan
 {
    public interface IKegiatanEditModel
    {
       ViewModels.Kegiatan Kegiatan { get; set; }
+
+      SelectList PenanggungJawab { get; set; }
+
+      SelectList KabupatenKota { get; set; }
+
+      SelectList SumberDana { get; set; }
    }
 }
