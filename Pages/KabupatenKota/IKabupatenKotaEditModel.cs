58801a1c961e15341337b7e7e622bc1243f7ff18
Martinus Suherman <martinus.suherman@gmail.com>
2021-04-20 12:32:30 +0700

    Update kabupaten/kota page model interface
    - Add provinsi select list

--- Pages/KabupatenKota/IKabupatenKotaEditModel.cs
+++ Pages/KabupatenKota/IKabupatenKotaEditModel.cs
@@ -1,7 +1,11 @@
+using Microsoft.AspNetCore.Mvc.Rendering;
+
 namespace DeputiTigaKemenpora.Pages.KabupatenKota
 {
    public interface IKabupatenKotaEditModel
    {
       ViewModels.KabupatenKota KabupatenKota { get; set; }
+
+      SelectList Provinsi { get; set; }
    }
 }
