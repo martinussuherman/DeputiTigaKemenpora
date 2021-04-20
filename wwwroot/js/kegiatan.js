2ea3e573d1a861975a601029212fa0eccc157309
Martinus Suherman <martinus.suherman@gmail.com>
2021-04-20 11:12:04 +0700

    Update kegiatan js
    - Remove unused onFocusById method
    - Reformat code

--- wwwroot/js/kegiatan.js
+++ wwwroot/js/kegiatan.js
@@ -1,29 +1,27 @@
-function onFocusById(elementId) {
-    var datepickerObject = document.getElementById(elementId).ej2_instances[0];
-    datepickerObject.show();
-}
-
 function onFocus(element) {
-    var datepickerObject = element.ej2_instances[0];
-    datepickerObject.show();
+   var datepickerObject = element.ej2_instances[0];
+   datepickerObject.show();
 }
 
 $(document).ready(function () {
-    $('.foto-kegiatan .btn-upload').click(function () {
-        process(this, 'foto-kegiatan');
-    });
-    $('.file-pendukung .btn-upload').click(function () {
-        process(this, 'file-pendukung');
-    });
-    $('.ada-kendala').change(function () {
-        if (this.checked) {
-            $('.kendala').removeAttr('disabled');
-        }
-        else {
-            $('.kendala').attr('disabled', 'disabled');
-        }
-    });
-    $('#tanggal-mulai, #tanggal-selesai').focus(function() {
-        onFocus( this );
-    });
+   $('.foto-kegiatan .btn-upload').click(function () {
+      process(this, 'foto-kegiatan');
+   });
+
+   $('.file-pendukung .btn-upload').click(function () {
+      process(this, 'file-pendukung');
+   });
+
+   $('.ada-kendala').change(function () {
+      if (this.checked) {
+         $('.kendala').removeAttr('disabled');
+         return;
+      }
+
+      $('.kendala').attr('disabled', 'disabled');
+   });
+
+   $('#tanggal-mulai, #tanggal-selesai').focus(function () {
+      onFocus(this);
+   });
 });
