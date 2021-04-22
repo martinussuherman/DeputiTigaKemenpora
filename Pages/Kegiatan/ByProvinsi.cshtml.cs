71d318c8b1b6412ca3f7ad56151b22a6fc40e837
Martinus Suherman <martinus.suherman@gmail.com>
2021-04-22 12:02:22 +0700

    Refactor: move duplicate logic to base class.
    Fix #3

--- Pages/Kegiatan/ByProvinsi.cshtml.cs
+++ Pages/Kegiatan/ByProvinsi.cshtml.cs
@@ -1,53 +1,28 @@
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using DeputiTigaKemenpora.Data;
-using DeputiTigaKemenpora.Identity;
-using DeputiTigaKemenpora.ViewModels;
-using Itm.Misc;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using P.Pager;
 
 namespace DeputiTigaKemenpora.Pages.Kegiatan
 {
-   public class ByProvinsiModel : CustomPageModel, IKegiatanIndexModel
+   public class ByProvinsiModel : KegiatanIndexModel
    {
       public ByProvinsiModel(
-          IAuthorizationService authorizationService,
-          ApplicationDbContext context)
+         IAuthorizationService authorizationService,
+         ApplicationDbContext context)
+         : base(authorizationService, context)
       {
-         _authorizationService = authorizationService;
-         _context = context;
-         Title = "Daftar Kegiatan";
-         PageTitle = "Kegiatan";
       }
 
-      public IPager<ViewModels.Kegiatan> Kegiatan { get; set; }
-
-      public bool IsUserCanDelete { get; set; }
-
-      public async Task OnGetAsync([FromQuery] byte prov = 0, [FromQuery] int page = 1)
+      public async Task OnGetAsync([FromQuery] byte? prov, [FromQuery] int page = 1)
       {
-         List<Models.Kegiatan> list = await _context.Kegiatan
-            .Include(e => e.PenanggungJawabNavigation)
-            .Include(e => e.KabupatenKota.Provinsi)
-            .Where(e => e.KabupatenKota.ProvinsiId == prov)
-            .OrderBy(e => e.KabupatenKota.Nama)
-            .ThenBy(e => e.Nama)
-            .ThenBy(e => e.TanggalMulai)
-            .AsNoTracking()
-            .ToListAsync();
-         Kegiatan = list
-            .ViewModelCopy<ViewModels.Kegiatan, Models.Kegiatan>(page);
-         AuthorizationResult result = _authorizationService.AuthorizeAsync(
-            User,
-            Permissions.Kegiatan.Delete).Result;
-         IsUserCanDelete = result.Succeeded;
-      }
+         if (prov == null)
+         {
+            await RetrieveData(page);
+            return;
+         }
 
-      private readonly IAuthorizationService _authorizationService;
-      private readonly ApplicationDbContext _context;
+         await RetrieveData(prov.Value, page);
+      }
    }
 }
--- Pages/Kegiatan/Index.cshtml.cs
+++ Pages/Kegiatan/Index.cshtml.cs
@@ -1,51 +1,22 @@
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using DeputiTigaKemenpora.Data;
-using DeputiTigaKemenpora.Identity;
-using DeputiTigaKemenpora.ViewModels;
-using Itm.Misc;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using P.Pager;
 
 namespace DeputiTigaKemenpora.Pages.Kegiatan
 {
-   public class IndexModel : CustomPageModel, IKegiatanIndexModel
+   public class IndexModel : KegiatanIndexModel
    {
       public IndexModel(
-          IAuthorizationService authorizationService,
-          ApplicationDbContext context)
+         IAuthorizationService authorizationService,
+         ApplicationDbContext context)
+         : base(authorizationService, context)
       {
-         _authorizationService = authorizationService;
-         _context = context;
-         Title = "Daftar Kegiatan";
-         PageTitle = "Kegiatan";
       }
-
-      public IPager<ViewModels.Kegiatan> Kegiatan { get; set; }
-
-      public bool IsUserCanDelete { get; set; }
 
       public async Task OnGetAsync([FromQuery] int page = 1)
       {
-         List<Models.Kegiatan> list = await _context.Kegiatan
-            .Include(e => e.PenanggungJawabNavigation)
-            .OrderBy(e => e.PenanggungJawab)
-            .ThenBy(e => e.Nama)
-            .ThenBy(e => e.Tempat)
-            .AsNoTracking()
-            .ToListAsync();
-         Kegiatan = list
-            .ViewModelCopy<ViewModels.Kegiatan, Models.Kegiatan>(page);
-         AuthorizationResult result = _authorizationService.AuthorizeAsync(
-            User,
-            Permissions.Kegiatan.Delete).Result;
-         IsUserCanDelete = result.Succeeded;
+         await RetrieveData(page);
       }
-
-      private readonly IAuthorizationService _authorizationService;
-      private readonly ApplicationDbContext _context;
    }
 }
