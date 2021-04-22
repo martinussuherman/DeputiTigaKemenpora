c3f372f5366efade7c6417423e0f60a7d56866f5
Martinus Suherman <martinus.suherman@gmail.com>
2021-04-22 15:24:34 +0700

    Create map data view model

---
+++ ViewModels/MapData.cs
@@ -0,0 +1,15 @@
+namespace DeputiTigaKemenpora.ViewModels
+{
+   public class MapData
+   {
+      public string Nama { get; set; }
+
+      public byte? Id { get; set; }
+
+      public int JumlahKegiatan { get; set; }
+
+      public decimal Lat { get; set; }
+
+      public decimal Long { get; set; }
+   }
+}
