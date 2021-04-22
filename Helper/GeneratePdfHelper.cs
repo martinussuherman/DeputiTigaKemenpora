928850f2bf69b94cc532bcf43e6a3709724a6098
Martinus Suherman <martinus.suherman@gmail.com>
2021-04-22 17:22:33 +0700

    Update generate pdf helper
    - Simplify String
    - Reformat code

--- Helper/GeneratePdfHelper.cs
+++ Helper/GeneratePdfHelper.cs
@@ -4,47 +4,47 @@ using Syncfusion.Pdf.Graphics;
 
 namespace DeputiTigaKemenpora
 {
-    public static class GeneratePdfHelper
-    {
-        public static string WebKitPath { get; set; }
+   public static class GeneratePdfHelper
+   {
+      public static string WebKitPath { get; set; }
 
-        public static string DefaultOutputFileName { get; set; }
+      public static string DefaultOutputFileName { get; set; }
 
-        public static int MarginTopMm { get; set; }
+      public static int MarginTopMm { get; set; }
 
-        public static int MarginBottomMm { get; set; }
+      public static int MarginBottomMm { get; set; }
 
-        public static int MarginLeftMm { get; set; }
+      public static int MarginLeftMm { get; set; }
 
-        public static int MarginRightMm { get; set; }
+      public static int MarginRightMm { get; set; }
 
-        public static float MarginTopPoints => ConvertMmToPoint(MarginTopMm);
+      public static float MarginTopPoints => ConvertMmToPoint(MarginTopMm);
 
-        public static float MarginBottomPoints => ConvertMmToPoint(MarginBottomMm);
+      public static float MarginBottomPoints => ConvertMmToPoint(MarginBottomMm);
 
-        public static float MarginLeftPoints => ConvertMmToPoint(MarginLeftMm);
+      public static float MarginLeftPoints => ConvertMmToPoint(MarginLeftMm);
 
-        public static float MarginRightPoints => ConvertMmToPoint(MarginRightMm);
+      public static float MarginRightPoints => ConvertMmToPoint(MarginRightMm);
 
-        public static float ConvertMmToPoint(int mm)
-        {
-            return converter.ConvertUnits(
-                mm,
-                PdfGraphicsUnit.Millimeter,
-                PdfGraphicsUnit.Point);
-        }
+      public static float ConvertMmToPoint(int mm)
+      {
+         return converter.ConvertUnits(
+            mm,
+            PdfGraphicsUnit.Millimeter,
+            PdfGraphicsUnit.Point);
+      }
 
-        public static int ReadMarginFromConfig(
-            IConfigurationSection section,
-            string subSectionName)
-        {
-            string value = section
-                .GetSection(subSectionName)
-                .Value;
+      public static int ReadMarginFromConfig(
+         IConfigurationSection section,
+         string subSectionName)
+      {
+         string value = section
+            .GetSection(subSectionName)
+            .Value;
 
-            return String.IsNullOrEmpty(value) ? 0 : Convert.ToInt32(value);
-        }
+         return string.IsNullOrEmpty(value) ? 0 : Convert.ToInt32(value);
+      }
 
-        private static readonly PdfUnitConverter converter = new PdfUnitConverter();
-    }
+      private static readonly PdfUnitConverter converter = new PdfUnitConverter();
+   }
 }
