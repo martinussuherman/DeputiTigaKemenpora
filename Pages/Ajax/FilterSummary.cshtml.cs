8bdddda7e9c5719c2fcc92f4a923cd2edba661d6
Martinus Suherman <martinus.suherman@gmail.com>
2021-04-23 11:23:53 +0700

    Update summary filter
    - Fix wrong group by property
    - Remove logger DI
    - Reformat code

--- Pages/Ajax/FilterSummary.cshtml.cs
+++ Pages/Ajax/FilterSummary.cshtml.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
@@ -8,87 +7,81 @@ using DeputiTigaKemenpora.ViewModels;
 using Itm.Misc;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Logging;
 
 namespace DeputiTigaKemenpora.Pages.Ajax
 {
-    public class FilterSummaryModel : CustomPageModel
-    {
-        public FilterSummaryModel(
-            ILogger<FilterSummaryModel> logger,
-            ApplicationDbContext context)
-        {
-            _logger = logger;
-            _context = context;
-            Title = "Filter Summary";
-        }
+   public class FilterSummaryModel : CustomPageModel
+   {
+      public FilterSummaryModel(ApplicationDbContext context)
+      {
+         _context = context;
+         Title = "Filter Summary";
+      }
 
-        public async Task<JsonResult> OnGetAsync(int tahun)
-        {
-            List<Models.PenanggungJawab> listPenanggungJawab = await _context.PenanggungJawab
-                .OrderBy(e => e.Nama)
-                .AsNoTracking()
-                .ToListAsync();
+      public async Task<JsonResult> OnGetAsync(int tahun)
+      {
+         List<Models.PenanggungJawab> listPenanggungJawab = await _context.PenanggungJawab
+            .OrderBy(e => e.Nama)
+            .AsNoTracking()
+            .ToListAsync();
 
-            IQueryable<Models.Kegiatan> query = _context.Kegiatan;
+         IQueryable<Models.Kegiatan> query = _context.Kegiatan;
 
-            if (tahun != 0)
+         if (tahun != 0)
+         {
+            query = query
+               .Where(e => e.TanggalMulai.Year == tahun);
+         }
+
+         List<SummaryKegiatan> sumInfo = await query
+            .GroupBy(e => e.PenanggungJawabId)
+            .Select(s => new SummaryKegiatan
             {
-                query = query
-                    .Where(e => e.TanggalMulai.Year == tahun);
-            }
+               IdPenanggungJawab = s.Key.Value,
+               JumlahKegiatan = s.Count(),
+               TotalPeserta = s.Sum(e => e.JumlahPeserta)
+            })
+            .AsNoTracking()
+            .ToListAsync();
 
-            List<SummaryKegiatan> sumInfo = await query
-                .GroupBy(e => e.PenanggungJawab)
-                .Select(s => new SummaryKegiatan
-                {
-                    IdPenanggungJawab = s.Key.Value,
-                    JumlahKegiatan = s.Count(),
-                    TotalPeserta = s.Sum(e => e.JumlahPeserta)
-                })
-                .AsNoTracking()
-                .ToListAsync();
+         foreach (SummaryKegiatan item in sumInfo)
+         {
+            Models.PenanggungJawab penanggungJawab = listPenanggungJawab
+               .Find(e => e.Id == item.IdPenanggungJawab);
+            item.NamaPenanggungJawab = penanggungJawab.Nama;
+         }
 
-            foreach (SummaryKegiatan item in sumInfo)
-            {
-                Models.PenanggungJawab penanggungJawab = listPenanggungJawab
-                    .Find(e => e.Id == item.IdPenanggungJawab);
-                item.NamaPenanggungJawab = penanggungJawab.Nama;
-            }
+         SummaryView result = new SummaryView
+         {
+            Summary = sumInfo.OrderBy(e => e.NamaPenanggungJawab),
+            GrandTotalKegiatan = sumInfo.Sum(e => e.JumlahKegiatan),
+            GrandTotalPeserta = sumInfo.Sum(e => e.TotalPeserta)
+         };
 
-            SummaryView result = new SummaryView
-            {
-                Summary = sumInfo.OrderBy(e => e.NamaPenanggungJawab),
-                GrandTotalKegiatan = sumInfo.Sum(e => e.JumlahKegiatan),
-                GrandTotalPeserta = sumInfo.Sum(e => e.TotalPeserta)
-            };
+         return new JsonResult(result);
+      }
 
-            return new JsonResult(result);
-        }
+      public class SummaryView
+      {
+         public IOrderedEnumerable<SummaryKegiatan> Summary { get; set; }
 
-        public class SummaryView
-        {
-            public IOrderedEnumerable<SummaryKegiatan> Summary { get; set; }
+         public int GrandTotalKegiatan { get; set; }
 
-            public int GrandTotalKegiatan { get; set; }
+         public long GrandTotalPeserta { get; set; }
 
-            public long GrandTotalPeserta { get; set; }
+         public string GrandTotalKegiatanView
+         {
+            get => GrandTotalKegiatan.ToString("##,#", info);
+         }
 
-            public string GrandTotalKegiatanView
-            {
-                get => GrandTotalKegiatan.ToString("##,#", info);
-            }
+         public string GrandTotalPesertaView
+         {
+            get => GrandTotalPeserta.ToString("##,#", info);
+         }
 
-            public string GrandTotalPesertaView
-            {
-                get => GrandTotalPeserta.ToString("##,#", info);
-            }
+         private static readonly CultureInfo info = CultureInfo.CreateSpecificCulture("id-ID");
+      }
 
-            private static readonly CultureInfo info = CultureInfo.CreateSpecificCulture("id-ID");
-        }
-
-        private readonly ILogger<FilterSummaryModel> _logger;
-
-        private readonly ApplicationDbContext _context;
-    }
+      private readonly ApplicationDbContext _context;
+   }
 }
