0e89d4e9c6c7cac2bb9debebcf6c510be913d81f
Martinus Suherman <martinus.suherman@gmail.com>
2021-04-26 11:21:23 +0700

    Update summary kegiatan view model
    - Remove property NamaPenanggungJawab, IdPenanggungJawab
    - Add property Nama, IdSumberDana, NamaSumberDana
    - Reformat code

--- ViewModels/SummaryKegiatan.cs
+++ ViewModels/SummaryKegiatan.cs
@@ -2,26 +2,44 @@ using System.Globalization;
 
 namespace DeputiTigaKemenpora.ViewModels
 {
-    public class SummaryKegiatan
-    {
-        public string NamaPenanggungJawab { get; set; }
+   public class SummaryKegiatan
+   {
+      public string Nama { get; set; }
 
-        public ushort IdPenanggungJawab { get; set; }
+      public byte? IdSumberDana { get; set; }
 
-        public int JumlahKegiatan { get; set; }
+      public string NamaSumberDana
+      {
+         get
+         {
+            return _namaSumberDana;
+         }
+         set
+         {
+            if (value == null)
+            {
+               value = string.Empty;
+            }
 
-        public string JumlahKegiatanView
-        {
-            get => JumlahKegiatan.ToString("##,#", info);
-        }
+            _namaSumberDana = value;
+         }
+      }
 
-        public long TotalPeserta { get; set; }
+      public int JumlahKegiatan { get; set; }
 
-        public string TotalPesertaView
-        {
-            get => TotalPeserta.ToString("##,#", info);
-        }
+      public string JumlahKegiatanView
+      {
+         get => JumlahKegiatan.ToString("##,#", info);
+      }
 
-        private static readonly CultureInfo info = CultureInfo.CreateSpecificCulture("id-ID");
-    }
+      public long TotalPeserta { get; set; }
+
+      public string TotalPesertaView
+      {
+         get => TotalPeserta.ToString("##,#", info);
+      }
+
+      private string _namaSumberDana;
+      private static readonly CultureInfo info = CultureInfo.CreateSpecificCulture("id-ID");
+   }
 }
