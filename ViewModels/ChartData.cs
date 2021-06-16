fea893697c9fea8b901465f848ca4b8aae6dc1d3
Martinus Suherman <martinus.suherman@gmail.com>
2021-06-16 10:21:53 +0700

    Create chart data view model

---
+++ ViewModels/ChartData.cs
@@ -0,0 +1,13 @@
+namespace DeputiTigaKemenpora.ViewModels
+{
+   public class ChartData
+   {
+      public int Id { get; set; }
+
+      public string Nama { get; set; }
+
+      public int JumlahKegiatan { get; set; }
+
+      public long JumlahPeserta { get; set; }
+   }
+}
