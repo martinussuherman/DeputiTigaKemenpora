e5cf965a5e5de72140245419023558e6943e9fd7
Martinus Suherman <martinus.suherman@gmail.com>
2022-07-06 12:05:11 +0700

    Update home.js:
    - Modify function DisplaySummary and DisplayTotal - check if tablesaw is active

--- wwwroot/js/home.js
+++ wwwroot/js/home.js
@@ -19,21 +19,61 @@ function AjaxSummaryKegiatan(tahunSummary) {
 }
 
 function DisplaySummary(data) {
-   return '<tr>' +
-      '<td>' + data.nama + '</td>' +
-      '<td>' + data.namaSumberDana + '</td>' +
-      '<td class="text-right pr-4">' + data.jumlahKegiatanView + '</td>' +
-      '<td class="text-right pr-4">' + data.totalPesertaView + '</td>' +
-      '</tr>';
+   if ($('#kegiatan-summary b.tablesaw-cell-label').length) {
+      return `
+         <tr>
+            <td>
+               <b class="tablesaw-cell-label">Nama</b>
+               <span class="tablesaw-cell-content">${data.nama}</span>
+            </td>
+            <td>
+               <b class="tablesaw-cell-label">Sumber Dana</b>
+               <span class="tablesaw-cell-content">${data.namaSumberDana}</span>
+            </td>
+            <td>
+               <b class="tablesaw-cell-label">Jumlah Kegiatan</b>
+               <span class="tablesaw-cell-content">${data.jumlahKegiatanView}</span>
+            </td>
+            <td>
+               <b class="tablesaw-cell-label">Total Peserta</b>
+               <span class="tablesaw-cell-content">${data.totalPesertaView}</span>
+            </td>
+         </tr>`;
+   }
+
+   return `
+      <tr>
+         <td>${data.nama}</td>
+         <td>${data.namaSumberDana}</td>
+         <td class="text-right pr-4">${data.jumlahKegiatanView}</td>
+         <td class="text-right pr-4">${data.totalPesertaView}</td>
+      </tr>`;
 }
 
 function DisplayTotal(data) {
-   return '<tr class="table-warning">' +
-      '<td>Total</td>' +
-      '<td></td>' +
-      '<td class="text-right pr-4">' + data.grandTotalKegiatanView + '</td>' +
-      '<td class="text-right pr-4">' + data.grandTotalPesertaView + '</td>' +
-      '</tr>';
+   if ($('#kegiatan-summary b.tablesaw-cell-label').length) {
+      return `
+         <tr class="table-warning">
+            <td>Total</td>
+            <td></td>
+            <td>
+               <b class="tablesaw-cell-label">Kegiatan</b>
+               <span class="tablesaw-cell-content">${data.grandTotalKegiatanView}</span>
+            </td>
+            <td>
+               <b class="tablesaw-cell-label">Peserta</b>
+               <span class="tablesaw-cell-content">${data.grandTotalPesertaView}</span>
+            </td>
+         </tr>`;
+   }
+
+   return `
+      <tr class="table-warning">
+         <td>Total</td>
+         <td></td>
+         <td class="text-right pr-4">${data.grandTotalKegiatanView}</td>
+         <td class="text-right pr-4">${data.grandTotalPesertaView}</td>
+      </tr>`;
 }
 
 $(document).ready(function () {
